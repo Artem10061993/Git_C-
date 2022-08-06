53c53e38f1afc8ef9a591e6d423f3b4d3d94118e
Artem Kandakov <Temofey1993@gmail.com>
2022-08-06 18:28:56 +0500

    Example_Condition

---
+++ Example_Condition/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользватедя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "артем")
+{
+    
+    Console.WriteLine("УРА Привет Артем!");
+
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
